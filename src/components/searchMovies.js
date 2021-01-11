d6ba3f593c11be8af5ae884a780d12a82bfb4509
Rustam <rustam905@gmail.com>
2021-01-10 17:12:49 -0800

    useState

--- src/components/searchMovies.js
+++ src/components/searchMovies.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function SearchMovies() {
+	//states- input querry, movies
+	const [query, setQuery] = useState("");
+	const [movie,setMovie] = useState([])
+	const searchMovie = async (e) => {
+		e.preventDefault();
+		const url = `https://api.themoviedb.org/3/search/movie?api_key=cdd3c5ce5ede207e5ca373b35395541c&language=en-US&query=%24%7B${query}%7D&page=1&include_adult=false`;
+		
+		try {
+			const res = await fetch(url);
+			const data = await res.json();
+			setMovie(data.results)
+		} catch (err) {
+			console.log(err);
+		}
+	};
+
 	return (
-		<form>
-			<div className='form-group'>
+		<form onSubmit={searchMovie}>
+			<div className='form form-group'>
 				<label htmlFor='query' className='label'>
 					Movie Name
 				</label>
@@ -12,9 +28,10 @@ export default function SearchMovies() {
 					type='text'
 					name='query'
 					placeholder='Movie'
-					autoComplete="off"
+					autoComplete='off'
+					value={query} onChange={(e)=> setQuery(e.target.value)}
 				/>
-				<button className='btn btn-primary' type='submit'>
+				<button className='button btn btn-primary' type='submit'>
 					Search
 				</button>
 			</div>
