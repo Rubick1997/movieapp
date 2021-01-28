ab7d4e29686510a2a0c11a60c98015b5ea449a87
Rustam <rustam905@gmail.com>
2021-01-28 14:58:24 -0800

    Director picture

--- src/components/creditComponent.js
+++ src/components/creditComponent.js
@@ -2,42 +2,47 @@ import React from "react";
 
 function Credits({ casts, crews }) {
 	return (
-        
-		<div>
-			<table>
-				<thead>
-					<tr>
-						<th scope='col'>Name</th>
-						<th scope='col'>Character</th>
-						<th scope='col'>Job</th>
-					</tr>
-				</thead>
-				<tbody>
-					{casts.slice(0, 10).map((actor) => (
-						<tr key={actor.id}>
-							<td>{actor.name}</td>
-							<td>{actor.character}</td>
-							<td>Actor</td>
-						</tr>
-					))}
-					{crews
-						.filter(function (person) {
-							return person.job === "Director";
-						})
-						.map((person) => (
-							<tr key={person.id}>
-								<td>{person.name}</td>
-								<td></td>
-								<td>{person.job}</td>
-								<img
-									id='poster'
-									src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${person.profile_path}`}
-									alt={person.name + " picture"}
-								/>
-							</tr>
-						))}
-				</tbody>
-			</table>
+		<div className='container'>
+			{crews
+				.filter(function (person) {
+					return person.job === "Director";
+				})
+				.map((person) => (
+					<div className='row'>
+						<table className='col'>
+							<thead>
+								<tr>
+									<th scope='col'>Name</th>
+									<th scope='col'>Character</th>
+									<th scope='col'>Job</th>
+								</tr>
+							</thead>
+							<tbody>
+								{casts.slice(0, 10).map((actor) => (
+									<tr key={actor.id}>
+										<td>{actor.name}</td>
+										<td>{actor.character}</td>
+										<td>Actor</td>
+									</tr>
+								))}
+
+								<tr key={person.id}>
+									<td>{person.name}</td>
+									<td></td>
+									<td>{person.job}</td>
+								</tr>
+							</tbody>
+						</table>
+						<div className='col'>
+							<img
+								id='poster'
+								src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${person.profile_path}`}
+								alt={person.name + " picture"}
+							/>
+							<h3 className='text-center'>{person.name}</h3>
+						</div>
+					</div>
+				))}
 		</div>
 	);
 }
